8122a9d49c99bfda644786dfcc22156e1c3f12e1
Ghon4 <m.ghoniem97@gmail.com>
2023-11-28 15:34:01 +0200

    uncomment the network layer and adding the movies base url and token

--- lib/data/configs/app_config.dart
+++ lib/data/configs/app_config.dart
@@ -1,39 +1,41 @@
 import 'package:flutter/foundation.dart';
 
 class AppConfig {
-  static const String defaultLocale = 'en';
+  // static const String defaultLocale = 'en';
+  //
+  // static const appleApiKey = '';
+  //
+  // static const googleApiKey = '';
 
-  static const appleApiKey = '';
-
-  static const googleApiKey = '';
-
-  static const entitlementKey = '';
+  static const apiKey = '6a222a0bf85f125fd558c29f1f9a6205';
+  static const authToken =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTIyMmEwYmY4NWYxMjVmZDU1OGMyOWYxZjlhNjIwNSIsInN1YiI6IjY1NjVlOGQ5ZDk1NDIwMDBmZTMzMzYzMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I5TzJhNPtcWT6uWTnZYrwbHFZEqLqgff5HR3S6YZYHg";
 
   static String get baseUrl {
     if (kReleaseMode) {
       /// TODO
       // Production environment
-      return '';
+      return 'https://api.themoviedb.org/3';
     } else if (kDebugMode) {
       // Test environment
-      return '';
+      return 'https://api.themoviedb.org/3';
     } else {
       // Development environment
-      return '';
+      return 'https://api.themoviedb.org/3';
     }
   }
 
-  static String get oneSignalAppId {
-    if (kReleaseMode) {
-      /// TODO
-      // Production environment
-      return '';
-    } else if (kDebugMode) {
-      // Test environment
-      return '';
-    } else {
-      // Development environment
-      return '';
-    }
-  }
+  // static String get oneSignalAppId {
+  //   if (kReleaseMode) {
+  //     /// TODO
+  //     // Production environment
+  //     return '';
+  //   } else if (kDebugMode) {
+  //     // Test environment
+  //     return '';
+  //   } else {
+  //     // Development environment
+  //     return '';
+  //   }
+  // }
 }
--- lib/data/data_providers/network/network_manager.dart
+++ lib/data/data_providers/network/network_manager.dart
@@ -1,100 +1,102 @@
-// import 'package:dio/dio.dart';
+import 'package:dio/dio.dart';
+
+import 'dio_initializer.dart';
 /// generic class for network manager with get, post, put, delete methods
-// class NetworkManager {
-//   late Dio _dio;
-//
-//   NetworkManager() {
-//     _dio = DioInitializer.dioInstance;
-//   }
-//
-//   Future<T> get<T>(String endpoint,
-//       {Map<String, dynamic>? queryParameters}) async {
-//     try {
-//       final response =
-//           await _dio.get(endpoint, queryParameters: queryParameters);
-//       return response as T;
-//     } on DioException catch (e) {
-//       throw _handleError(e);
-//     }
-//   }
-//
-//   Future<T> post<T>(String endpoint, dynamic data,
-//       {Map<String, dynamic>? queryParameters}) async {
-//     try {
-//       final response = await _dio.post(endpoint,
-//           data: data, queryParameters: queryParameters);
-//       return response as T;
-//     } on DioException catch (e) {
-//       // return e as T;
-//       throw _handleError(e);
-//     }
-//   }
-//
-//   Future<T> put<T>(String endpoint, dynamic data,
-//       {Map<String, dynamic>? queryParameters}) async {
-//     try {
-//       final response = await _dio.put(endpoint,
-//           data: data, queryParameters: queryParameters);
-//       return response as T;
-//     } catch (e) {
-//       throw _handleError(e);
-//     }
-//   }
-//
-//   Future<T> delete<T>(String endpoint,
-//       {Map<String, dynamic>? queryParameters}) async {
-//     try {
-//       final response =
-//           await _dio.delete(endpoint, queryParameters: queryParameters);
-//       return response as T;
-//     } catch (e) {
-//       throw _handleError(e);
-//     }
-//   }
-//
-//   dynamic _handleError(dynamic error) {
-//     if (error is DioException) {
-//       if (error.response != null) {
-//         final responseData = error.response!.data;
-//
-//         if (responseData != null) {
-//           final messageData = responseData['message'];
-//           final statusCode = responseData['statusCode'];
-//
-//           if (messageData != null &&
-//               // messageData is List<dynamic> &&
-//               messageData.isNotEmpty) {
-//             final message;
-//             if (messageData is List) {
-//               message = messageData[0];
-//             } else {
-//               message = messageData;
-//             }
-//
-//             throw NetworkException(message, statusCode);
-//           } else {
-//             throw NetworkException(
-//                 "No message found in response data.", statusCode);
-//           }
-//         } else {
-//           throw NetworkException(
-//               "Response data is null.", error.response!.statusCode!);
-//         }
-//       } else {
-//         throw NetworkException(
-//             "No response from server.", error.response!.statusCode!);
-//       }
-//     }
-//     throw error;
-//   }
-// }
-//
-// class NetworkException implements Exception {
-//   final String message;
-//   final int statusCode;
-//   NetworkException(this.message, this.statusCode);
-//   @override
-//   String toString() {
-//     return message;
-//   }
-// }
+class NetworkManager {
+  late Dio _dio;
+
+  NetworkManager() {
+    _dio = DioInitializer.dioInstance;
+  }
+
+  Future<T> get<T>(String endpoint,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response =
+          await _dio.get(endpoint, queryParameters: queryParameters);
+      return response as T;
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<T> post<T>(String endpoint, dynamic data,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.post(endpoint,
+          data: data, queryParameters: queryParameters);
+      return response as T;
+    } on DioException catch (e) {
+      // return e as T;
+      throw _handleError(e);
+    }
+  }
+
+  Future<T> put<T>(String endpoint, dynamic data,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.put(endpoint,
+          data: data, queryParameters: queryParameters);
+      return response as T;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<T> delete<T>(String endpoint,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response =
+          await _dio.delete(endpoint, queryParameters: queryParameters);
+      return response as T;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _handleError(dynamic error) {
+    if (error is DioException) {
+      if (error.response != null) {
+        final responseData = error.response!.data;
+
+        if (responseData != null) {
+          final messageData = responseData['message'];
+          final statusCode = responseData['statusCode'];
+
+          if (messageData != null &&
+              // messageData is List<dynamic> &&
+              messageData.isNotEmpty) {
+            final message;
+            if (messageData is List) {
+              message = messageData[0];
+            } else {
+              message = messageData;
+            }
+
+            throw NetworkException(message, statusCode);
+          } else {
+            throw NetworkException(
+                "No message found in response data.", statusCode);
+          }
+        } else {
+          throw NetworkException(
+              "Response data is null.", error.response!.statusCode!);
+        }
+      } else {
+        throw NetworkException(
+            "No response from server.", error.response!.statusCode!);
+      }
+    }
+    throw error;
+  }
+}
+
+class NetworkException implements Exception {
+  final String message;
+  final int statusCode;
+  NetworkException(this.message, this.statusCode);
+  @override
+  String toString() {
+    return message;
+  }
+}
